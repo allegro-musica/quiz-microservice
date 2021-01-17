d8b43eb76772308151c8cbe4d7583c12cd1c3bb0
Waterball <waterballs12@gmail.com>
2021-01-17 17:32:10 +0000

    Add new protos and endpoints

--- Quiz.API/Services/QuizerService.cs
+++ Quiz.API/Services/QuizerService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Quiz.API;
 using Quizzer.Domain.Entities;
+using Quizzer.Domain.Exceptions;
 using Quizzer.Infrastructure;
 
 namespace Quizzer.API.Services
@@ -17,6 +20,37 @@ namespace Quizzer.API.Services
             _manager = manager;
         }
 
+        public override Task<Empty> JoinGame(JoinGameRequest request, ServerCallContext context)
+        {
+            _manager.JoinGame(request.Id, request.User);
+
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<QuizExistResponse> QuizExist(QuizExistRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var quiz = _manager.TryGetQuiz(request.Id);
+
+                return Task.FromResult(new QuizExistResponse()
+                {
+                    Quiz = new QuizData()
+                    {
+                        Description = quiz.Quiz.Description,
+                        ImageUrl = quiz.Quiz.ImageUrl,
+                        Title = quiz.Quiz.Title
+                    }
+                });
+            }
+            catch (GameNotFoundException)
+            {
+                context.Status = new Status(StatusCode.NotFound, "Quiz does not exist");
+
+                return Task.FromResult(new QuizExistResponse());
+            }
+        }
+
         public override Task<QuizCreatedResponse> CreateGame(QuizCreateRequest request, ServerCallContext context)
         {
             var quiz = new Domain.Entities.Quiz()
@@ -24,9 +58,11 @@ namespace Quizzer.API.Services
                 Title = request.Quiz.Title,
                 Description = request.Quiz.Description,
                 ImageUrl = request.Quiz.ImageUrl,
+                Users = new List<string>(),
                 Questions = request.Quiz.Questions.Select(x => new Question()
                 {
-                    Timeout = x.Timeout
+                    Timeout = x.Timeout,
+                    Title = x.Title
                 }).ToList()
             };
 
@@ -50,6 +86,22 @@ namespace Quizzer.API.Services
             };
 
             return Task.FromResult(response);
+        }
+
+        public override async Task<Empty> NextQuestion(NextQuestionRequest request, ServerCallContext context)
+        {
+            var game = _manager.TryGetQuiz(request.Id);
+
+            await _manager.StartQuestion(game.Id, game.CurrentQuestion);
+
+            return new Empty();
+        }
+
+        public override async Task<Empty> StartGame(QuizStartRequest request, ServerCallContext context)
+        {
+            await _manager.Start(request.Id);
+
+            return new Empty();
         }
     }
 }
---
+++ Quiz.Infrastructure/Extensions/EventBusExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Quizzer.Infrastructure.Extensions
+{
+    public static class EventBusExtensions
+    {
+        public static IServiceCollection AddBus(this IServiceCollection services, string connection)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(connection);
+                });
+            });
+
+            return services;
+        }
+    }
+}
