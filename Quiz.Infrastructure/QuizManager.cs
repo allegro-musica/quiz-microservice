5011bbfcea8b688650f646abdd0aa0d1421ddff6
Waterball <waterballs12@gmail.com>
2021-01-17 17:32:35 +0000

    Add temporary methods for managing the quiz

--- Quiz.Infrastructure/QuizManager.cs
+++ Quiz.Infrastructure/QuizManager.cs
@@ -1,19 +1,51 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using MassTransit;
 using Quizzer.Domain.Entities;
+using Quizzer.Domain.Events;
 using Quizzer.Domain.Exceptions;
 
 namespace Quizzer.Infrastructure
 {
     public class QuizManager
     {
+        private readonly IBusControl _bus;
+
+        public QuizManager(IBusControl bus)
+        {
+            _bus = bus;
+        }
 
         private readonly ConcurrentDictionary<ulong, QuizGame> _runningGame = new ();
 
+        private readonly ConcurrentDictionary<ulong, Quiz> _runningQuiz = new();
 
         //public bool Add(string game) => _runningGame.Add(userId);
         //public bool Remove(ulong userId) => _runningGame.TryRemove(userId);
         public void Clear() => _runningGame.Clear();
+
+        public QuizGame TryGetQuiz(ulong id)
+        {
+            if (!_runningGame.TryGetValue(id, out QuizGame game))
+            {
+                throw new GameNotFoundException(id);
+            }
+
+            return game;
+        }
+
+        /// <summary>
+        /// Join a user in the game
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user"></param>
+        public void JoinGame(ulong id, string user)
+        {
+            var game = TryGetQuiz(id);
+
+            game.Quiz.Users.Add(user);
+        }
 
         /// <summary>
         /// Create new game
@@ -22,7 +54,6 @@ namespace Quizzer.Infrastructure
         /// <returns></returns>
         public QuizGame CreateNew(QuizGame game)
         {
-            Console.WriteLine("Creating new game");
             _runningGame.TryAdd(game.Id, game);
 
             return game;
@@ -36,12 +67,52 @@ namespace Quizzer.Infrastructure
         /// <returns></returns>
         public QuizGame EndGame(ulong id)
         {
-            if (!_runningGame.TryGetValue(id, out QuizGame game))
+            return TryGetQuiz(id);
+        }
+
+        public async Task Start(ulong id)
+        {
+            var game = TryGetQuiz(id);
+
+            _runningQuiz.TryAdd(id, game.Quiz);
+
+            var gameStartedEvent = new GameStartedEvent()
             {
-                throw new GameNotFoundException(id);
+                GameId = id,
+                Users = game.Quiz.Users
+            };
+
+            await _bus.Publish(gameStartedEvent);
+
+            await Task.Delay(500);
+
+            await StartQuestion(id, game.CurrentQuestion);
+        }
+
+        public async Task<Question> StartQuestion(ulong quizId, int index)
+        {
+            var quiz = TryGetQuiz(quizId);
+
+            if (quiz.Quiz.Questions.Count <= index)
+            {
+                await _bus.Publish(new GameEndedEvent() {GameId = quizId});
+                return null;
             }
 
-            return game;
+            var question = quiz.Quiz.Questions[index];
+
+            if (question == null) return null; // Throw exception 
+
+            quiz.CurrentQuestion = quiz.CurrentQuestion += 1;
+
+            var questionStartedEvent = new QuestionStartedEvent()
+            {
+                Title = question.Title
+            };
+
+            await _bus.Publish(questionStartedEvent);
+
+            return question;
         }
     }
 }
