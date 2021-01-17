14329e4f9dab001c9853330f7b7e9f15257ab1ec
Waterball <waterballs12@gmail.com>
2021-01-17 17:31:10 +0000

    Configure masstransit

--- Quiz.API/Startup.cs
+++ Quiz.API/Startup.cs
@@ -1,21 +1,30 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Quizzer.API.Services;
 using Quizzer.Infrastructure;
+using Quizzer.Infrastructure.Extensions;
 
 namespace Quizzer.API
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
-
+            services.AddBus(_config.GetValue<string>("RabbitMQConnection", "rabbitmq://localhost"));
             services.AddSingleton<QuizManager>();
         }
 
