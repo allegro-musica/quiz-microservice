be651a02d66bc1484c38e4349ec75c03512e11c7
Waterball <waterballs12@gmail.com>
2021-01-17 17:31:28 +0000

    Add events

---
+++ Quiz.Domain/Events/GameEndedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzer.Domain.Events
+{
+    public class GameEndedEvent
+    {
+        public ulong GameId { get; init; }
+    }
+}
---
+++ Quiz.Domain/Events/GameStartedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzer.Domain.Events
+{
+    public record GameStartedEvent
+    {
+        public ulong GameId { get; init; }
+        public IList<string> Users { get; init; }
+    }
+}
